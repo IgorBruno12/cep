674017e527c516fe9873f4112b485c59a6ec41d1
Igor Bruno <84923785+IgorBruno12@users.noreply.github.com>
2023-05-27 16:06:59 -0300

    atualização login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 const routes: Routes = [
   {
-    path:'',component:LoginComponent
+    path:'index',component:LoginComponent
+  },
+  {
+    path:'cadastro',component:CadastroComponent
   }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
